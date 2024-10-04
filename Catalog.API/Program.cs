83e4a011b0d94c8176cc27ca95e510796014fc12
Matias Salomon <matias.salomon@clt.com.py>
2024-10-04 16:45:59 -0400

    64. Connect Postgres DB from local Catalog Microservices and send POST request

--- Catalog.API/Program.cs
+++ Catalog.API/Program.cs
@@ -1,11 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);}
+var builder = WebApplication.CreateBuilder(args);
 
 IConfiguration config = builder.Configuration;
 
 builder.Services.AddCarter();
 builder.Services.AddMediatR(conf => conf.RegisterServicesFromAssembly(typeof(Program).Assembly));
 builder.Services.AddMarten(opt => opt.Connection(config.GetConnectionString("Database")!)).UseLightweightSessions();
-
+    
 var app = builder.Build();
 
 app.MapCarter();
