23f750c23c8c2b8f6f4296c2ed63d04db11b3743
Matias Salomon <matias.salomon@clt.com.py>
2024-10-07 14:32:52 -0300

    66. Develop GetProducts GET Endpoint with Minimal Apis and Carter

--- Catalog.API/Products/List/Endpint.cs
+++ Catalog.API/Products/List/Endpint.cs
@@ -1,6 +1,26 @@
-﻿namespace Catalog.API.Products.List
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.List;
+
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+
+public class GetProductsEndpint : ICarterModule
 {
-    public class Endpint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProductsResponse>();
+            
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Product");
     }
 }
