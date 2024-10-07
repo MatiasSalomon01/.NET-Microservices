ed242939b19232584c65c6d59d6e43c77f77dc1f
Matias Salomon <matias.salomon@clt.com.py>
2024-10-07 15:39:12 -0300

    79. Develop DeleteProduct DELETE Endpoint with Minimal Apis and Carter

--- Catalog.API/Products/Delete/Endpoint.cs
+++ Catalog.API/Products/Delete/Endpoint.cs
@@ -1,16 +1,14 @@
 ﻿namespace Catalog.API.Products.Delete;
 
-public record DeleteProductRequest(Guid Id);
 public record DeleteProductResponse(bool IsSuccess);
 
 public class DeleteProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapDelete("/products", async (DeleteProductRequest request, ISender sender) =>
+        app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
         {
-            var command = request.Adapt<DeleteProductCommand>();
-            var result = await sender.Send(command);
+            var result = await sender.Send(new DeleteProductCommand(id));
             var response = result.Adapt<DeleteProductResponse>();
 
             return Results.Ok(response);
