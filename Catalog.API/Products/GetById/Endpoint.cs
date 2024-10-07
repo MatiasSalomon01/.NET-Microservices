e12073c2a42fa40433c6a444110147ede2988d55
Matias Salomon <matias.salomon@clt.com.py>
2024-10-07 14:55:35 -0300

    69. Develop GetProductById Handler with CQRS and MediatR

--- Catalog.API/Products/GetById/Endpoint.cs
+++ Catalog.API/Products/GetById/Endpoint.cs
@@ -1,5 +1,25 @@
-﻿namespace Catalog.API.Products.GetById;
+﻿
+using Catalog.API.Products.CreateProduct;
 
-public class Endpoint
+namespace Catalog.API.Products.GetById;
+
+public record GetProductByIdResponse(Product product);
+
+public class GetProductByIdEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
+    }
 }
