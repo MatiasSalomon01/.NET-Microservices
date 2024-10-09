7237e47f52b3add855bc991d05cd93dd99194160
Matias Salomon <matias.salomon@clt.com.py>
2024-10-09 09:42:01 -0300

    85. Validation Pipeline Behaviour with MediatR using Fluent Validation Library

--- Catalog.API/Program.cs
+++ Catalog.API/Program.cs
@@ -1,11 +1,18 @@
+using BuildingBlocks.Behaviours;
+
 var builder = WebApplication.CreateBuilder(args);
 
 IConfiguration config = builder.Configuration;
 
 builder.Services.AddCarter();
-builder.Services.AddMediatR(conf => conf.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddMediatR(conf =>
+{
+    conf.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    conf.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+});
 builder.Services.AddMarten(opt => opt.Connection(config.GetConnectionString("Database")!)).UseLightweightSessions();
-    
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
 var app = builder.Build();
 
 app.MapCarter();
