5e0e7a24b883784d9a1934943309b3ac9dd581d4
Matias Salomon <matias.salomon@clt.com.py>
2024-10-09 11:17:08 -0300

    93. Test Logging Behaviour for all MediatR Requests on Catalog.API microservices

--- Catalog.API/Products/Get/Handler.cs
+++ Catalog.API/Products/Get/Handler.cs
@@ -5,12 +5,10 @@ public record GetProductsQuery(): IQuery<GetProductsResult>;
 
 public record GetProductsResult(IEnumerable<Product> Products);
 
-internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IRequestHandler<GetProductsQuery, GetProductsResult>
+internal class GetProductsQueryHandler(IDocumentSession session) : IRequestHandler<GetProductsQuery, GetProductsResult>
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        logger.LogInformation("GetProductsQueryHandler.Hanlder called with {@Query}", query);
-
         var products = await session.Query<Product>().ToListAsync(cancellationToken);
 
         return new GetProductsResult(products);
--- Catalog.API/Products/GetByCategory/Handler.cs
+++ Catalog.API/Products/GetByCategory/Handler.cs
@@ -4,12 +4,10 @@ public record GetProductByCategoryQuery(string Category):IQuery<GetProductByCate
 
 public record GetProductByCategoryResult(IEnumerable<Product> Products);
 
-public class GetProductByCategoryHandler(IDocumentSession session, ILogger<GetProductByCategoryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+public class GetProductByCategoryHandler(IDocumentSession session) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
 {
     public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
     {
-        logger.LogInformation("GetProductByCategoryHandler.Handler called with {@GetProductByCategoryQuery}", query);
-
         var products = await session.Query<Product>()
             .Where(p => p.Category.Contains(query.Category))
             .ToListAsync(cancellationToken);
