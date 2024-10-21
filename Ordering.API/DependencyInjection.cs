5ae03428ad88187b46e3da1af4ebd48dc9ef824b
Matias Salomon <matias.salomon@clt.com.py>
2024-10-21 11:36:30 -0300

    187. Adding Dependency Injections Between Clean Architecture Layers

---
+++ Ordering.API/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWebApiServices(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static WebApplication UseWebApiServices(this WebApplication app)
+    {
+        return app;
+    }
+}
--- Ordering.API/Program.cs
+++ Ordering.API/Program.cs
@@ -1,8 +1,16 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddWebApiServices()
+                .AddInfrastructureServices(builder.Configuration)
+                .AddApplicationServices();
 
 var app = builder.Build();
 
