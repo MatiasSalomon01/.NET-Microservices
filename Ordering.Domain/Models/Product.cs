4e135111b9b77d48cf15d6460d3e2d9f358446c8
Matias Salomon <matias.salomon@clt.com.py>
2024-10-21 13:04:47 -0300

    197. Develop DDD Rich-domain model for Order Entities in Order.Domain Layer

--- Ordering.Domain/Models/Product.cs
+++ Ordering.Domain/Models/Product.cs
@@ -4,4 +4,12 @@ public class Product : Entity<ProductId>
 {
     public string Name { get; private set; }
     public decimal Price { get; private set; }
+
+    public static Product Create(ProductId id, string name, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        return new Product { Id = id, Name = name, Price = price };
+    }
 }
